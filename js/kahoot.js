bab334639c04856d131ad9590e34ffc37ae24062
CristinaShuron <shuronster@gmail.com>
2023-01-14 14:13:57 +0200

    kahoot

---
+++ js/kahoot.js
@@ -0,0 +1,22 @@
+konsole.log(NaN === NaN);// false ВИНЯТОК!!!!!!!!!!!!!!!
+konsole.log('5' - true); // 5-1=4 true
+konsole.log('5' + 5);//5 далі записує наступну цифру 55 -конкатинація
+konsole.log(3 > 2 > 1);// false/ 3>2 true, true>1, 1>1 false
+konsole.log('4' == [4]); //true == true
+konsole.log('2' > '12');// порівнює рядки true,
+konsole.log(Boolean('0')); // рядoк true,
+konsole.log('10' == 10); //10 true
+konsole.log(Boolean(-5)); //-5 - true/всі числа true,0-false  
+konsole.log(Number('0'));// Number - 0
+konsole.log(Boolean(0));//0-false  
+konsole.log('Ежик' || 'еживика'); // true||true, ЧИ-повертає перше true
+konsole.log('5' - 5);//0
+konsole.log('10' > 5); //true
+konsole.log(true && 0 &&'еживика');// 0, true&&0=true&&false 0&&'еживика'=0,false
+konsole.log('Ежик' > 'еживика');
+konsole.log();
+konsole.log();
+konsole.log();
+konsole.log();
+konsole.log();
+konsole.log();
